1fcb6564889667c6297633cf5c69fb24b775890c
OutZzzz <outzz@yandex.ru>
2026-02-05 11:15:38 +0300

    feat: перенес css на модули

--- homework/src/components/Title/Title.jsx
+++ homework/src/components/Title/Title.jsx
@@ -1,9 +1,9 @@
-import './Title.css'
+import styles from './Title.module.css'
 
 function Title({title}) {
   
   return (
-    <h1 className='header-title'>{title}</h1>
+    <h1 className={styles['header-title']}>{title}</h1>
   )
 }
 
