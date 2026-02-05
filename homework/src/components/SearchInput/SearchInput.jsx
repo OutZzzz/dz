2147fb1c40a531a5f594788f5e0dfbccf6f2ec91
OutZzzz <outzz@yandex.ru>
2026-02-05 11:39:06 +0300

    fix: исправил класс

--- homework/src/components/SearchInput/SearchInput.jsx
+++ homework/src/components/SearchInput/SearchInput.jsx
@@ -4,7 +4,7 @@ function SearchInput({value, onChange, placeholder}) {
   
   return (
     <div className={styles['input-wrapper']}>
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className='search-icon'>
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles['search-icon']}>
           <path d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z" stroke="#475069" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
           <path d="M22 22L20 20" stroke="#475069" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
